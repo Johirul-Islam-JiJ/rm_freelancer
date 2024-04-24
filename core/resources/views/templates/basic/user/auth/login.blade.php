460363350ebb2559da5b0e0845f9a89a5e67a587
Johirul Islam <89728940+Johirul-Islam-JiJ@users.noreply.github.com>
2024-04-25 00:58:40 +0600

    Social login Design Added

--- core/resources/views/templates/basic/user/auth/login.blade.php
+++ core/resources/views/templates/basic/user/auth/login.blade.php
@@ -52,9 +52,68 @@
                                 </div>
                             </div>
                         </form>
+
+                        <div class="d-flex flex-wrap gap-3">
+                            <a href="https://script.viserlab.com/apointer/user/social-login/facebook"
+                                class="btn btn-outline-facebook btn-sm flex-grow-1">
+                                <span class="me-1"><i class="la la-facebook"></i></span>
+                                Facebook </a>
+                            <a href="https://script.viserlab.com/apointer/user/social-login/google"
+                                class="btn btn-outline-google btn-sm flex-grow-1">
+                                <span class="me-1"><i class="lab la-google"></i></span>
+                                Google </a>
+                            <a href="https://script.viserlab.com/apointer/user/social-login/linkedin"
+                                class="btn btn-outline-linkedin btn-sm flex-grow-1">
+                                <span class="me-1"><i class="la la-linkedin"></i></span>
+                                Linkedin </a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </section>
 @endsection
+
+@push('style')
+    <style>
+        .btn-outline-facebook {
+            border-color: #395498;
+            background-color: transparent;
+            color: #395498 !important;
+        }
+
+        .btn-outline-facebook:hover {
+            border-color: #395498;
+            color: #fff !important;
+            background-color: #395498;
+        }
+
+        .me-1 {
+            margin-right: .25rem !important;
+        }
+
+        .btn-outline-google {
+            border-color: #D64937;
+            background-color: transparent;
+            color: #D64937 !important;
+        }
+
+        .btn-outline-google:hover {
+            border-color: #D64937;
+            color: #fff !important;
+            background-color: #D64937;
+        }
+
+        .btn-outline-linkedin {
+            border-color: #0077B5;
+            background-color: transparent;
+            color: #0077B5 !important;
+        }
+
+        .btn-outline-linkedin:hover {
+            border-color: #0077B5;
+            color: #fff !important;
+            background-color: #0077B5;
+        }
+    </style>
+@endpush
