1f60028735af1ee00e85d4d512db3ddcb15b2007
Johirul Islam <89728940+Johirul-Islam-JiJ@users.noreply.github.com>
2024-05-14 02:54:02 +0600

    Design Update

--- core/resources/views/templates/basic/home.blade.php
+++ core/resources/views/templates/basic/home.blade.php
@@ -8,7 +8,7 @@
                     <div class="section-inner">
                         <div class="item-section">
                             <div class="container">
-                                @include($activeTemplate.'partials.top_filter')
+                                {{-- @include($activeTemplate.'partials.top_filter')
                                 <div class="item-bottom-area">
                                     <div class="row justify-content-center mb-30-none">
                                         <div class="col-xl-9 col-lg-9 mb-30">
@@ -27,7 +27,65 @@
                                         </div>
                                        @include($activeTemplate.'partials.filter')
                                     </div>
-                                </div>
+                                </div> --}}
+
+
+                                <div class="item-bottom-area">
+                                    <div class="row justify-content-center mb-30-none">
+                                        <div class="col-xl-12 col-lg-12 mb-30">
+                                            <h1 class="ptb-40 mt-40 bg--base text-center">@lang('Featured')</h1>
+                                            <div class="item-card-wrapper grid-view">
+                                                @forelse($products as $product)
+                                                    <x-item :product="$product" :type="$type"/>
+                                                @empty
+                                                    <x-basic-empty-message />
+                                                @endforelse
+                                            </div>
+                                            @if ($products->hasPages())
+                                            <nav>
+                                                {{ paginateLinks($products)}}
+                                            </nav>
+                                            @endif
+                                        </div>
+                                    </div>
+
+                                    <div class="row justify-content-center mb-30-none">
+                                        <div class="col-xl-12 col-lg-12 mb-30">
+                                            <h1 class="ptb-40 mt-40 bg--base text-center">@lang('SEO')</h1>
+                                            <div class="item-card-wrapper grid-view">
+                                                @forelse($products as $product)
+                                                    <x-item :product="$product" :type="$type"/>
+                                                @empty
+                                                    <x-basic-empty-message />
+                                                @endforelse
+                                            </div>
+                                            @if ($products->hasPages())
+                                            <nav>
+                                                {{ paginateLinks($products)}}
+                                            </nav>
+                                            @endif
+                                        </div>
+                                    </div>
+
+                                    <div class="row justify-content-center mb-30-none">
+                                        <div class="col-xl-12 col-lg-12 mb-30">
+                                            <h1 class="ptb-40 mt-40 bg--base text-center">@lang('Digital Marketing')</h1>
+                                            <div class="item-card-wrapper grid-view">
+                                                @forelse($products as $product)
+                                                    <x-item :product="$product" :type="$type"/>
+                                                @empty
+                                                    <x-basic-empty-message />
+                                                @endforelse
+                                            </div>
+                                            @if ($products->hasPages())
+                                            <nav>
+                                                {{ paginateLinks($products)}}
+                                            </nav>
+                                            @endif
+                                        </div>
+                                    </div>
+                                </div> 
+
                             </div>
                         </div>
                     </div>
